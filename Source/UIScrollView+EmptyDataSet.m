71670a5096b5950b468c58bc70e09e9aab247aa1
lirazshilk@gmail.com <lirazshilk@gmail.com>
2018-07-03 13:21:50 -0500

    Removed Method Swizzling completely - since it is causing crashes

--- Source/UIScrollView+EmptyDataSet.m
+++ Source/UIScrollView+EmptyDataSet.m
@@ -398,13 +398,7 @@ static char const * const kEmptyDataSetView =       "emptyDataSetView";
     
     objc_setAssociatedObject(self, kEmptyDataSetSource, [[DZNWeakObjectContainer alloc] initWithWeakObject:datasource], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     
-    // We add method sizzling for injecting -dzn_reloadData implementation to the native -reloadData implementation
-    [self swizzleIfPossible:@selector(reloadData)];
-    
-    // Exclusively for UITableView, we also inject -dzn_reloadData to -endUpdates
-    if ([self isKindOfClass:[UITableView class]]) {
-        [self swizzleIfPossible:@selector(endUpdates)];
-    }
+    [self reloadEmptyDataSet];
 }
 
 - (void)setEmptyDataSetDelegate:(id<DZNEmptyDataSetDelegate>)delegate
@@ -576,110 +570,6 @@ static char const * const kEmptyDataSetView =       "emptyDataSetView";
     
     // Notifies that the empty dataset view did disappear
     [self dzn_didDisappear];
-}
-
-
-#pragma mark - Method Swizzling
-
-static NSMutableDictionary *_impLookupTable;
-static NSString *const DZNSwizzleInfoPointerKey = @"pointer";
-static NSString *const DZNSwizzleInfoOwnerKey = @"owner";
-static NSString *const DZNSwizzleInfoSelectorKey = @"selector";
-
-// Based on Bryce Buchanan's swizzling technique http://blog.newrelic.com/2014/04/16/right-way-to-swizzle/
-// And Juzzin's ideas https://github.com/juzzin/JUSEmptyViewController
-
-void dzn_original_implementation(id self, SEL _cmd)
-{
-    // Fetch original implementation from lookup table
-    Class baseClass = dzn_baseClassToSwizzleForTarget(self);
-    NSString *key = dzn_implementationKey(baseClass, _cmd);
-    
-    NSDictionary *swizzleInfo = [_impLookupTable objectForKey:key];
-    NSValue *impValue = [swizzleInfo valueForKey:DZNSwizzleInfoPointerKey];
-    
-    IMP impPointer = [impValue pointerValue];
-    
-    // We then inject the additional implementation for reloading the empty dataset
-    // Doing it before calling the original implementation does update the 'isEmptyDataSetVisible' flag on time.
-    [self dzn_reloadEmptyDataSet];
-    
-    // If found, call original implementation
-    if (impPointer) {
-        ((void(*)(id,SEL))impPointer)(self,_cmd);
-    }
-}
-
-NSString *dzn_implementationKey(Class class, SEL selector)
-{
-    if (!class || !selector) {
-        return nil;
-    }
-    
-    NSString *className = NSStringFromClass([class class]);
-    
-    NSString *selectorName = NSStringFromSelector(selector);
-    return [NSString stringWithFormat:@"%@_%@",className,selectorName];
-}
-
-Class dzn_baseClassToSwizzleForTarget(id target)
-{
-    if ([target isKindOfClass:[UITableView class]]) {
-        return [UITableView class];
-    }
-    else if ([target isKindOfClass:[UICollectionView class]]) {
-        return [UICollectionView class];
-    }
-    else if ([target isKindOfClass:[UIScrollView class]]) {
-        return [UIScrollView class];
-    }
-    
-    return nil;
-}
-
-- (void)swizzleIfPossible:(SEL)selector
-{
-    // Check if the target responds to selector
-    if (![self respondsToSelector:selector]) {
-        return;
-    }
-    
-    // Create the lookup table
-    if (!_impLookupTable) {
-        _impLookupTable = [[NSMutableDictionary alloc] initWithCapacity:3]; // 3 represent the supported base classes
-    }
-    
-    // We make sure that setImplementation is called once per class kind, UITableView or UICollectionView.
-    for (NSDictionary *info in [_impLookupTable allValues]) {
-        Class class = [info objectForKey:DZNSwizzleInfoOwnerKey];
-        NSString *selectorName = [info objectForKey:DZNSwizzleInfoSelectorKey];
-        
-        if ([selectorName isEqualToString:NSStringFromSelector(selector)]) {
-            if ([self isKindOfClass:class]) {
-                return;
-            }
-        }
-    }
-    
-    Class baseClass = dzn_baseClassToSwizzleForTarget(self);
-    NSString *key = dzn_implementationKey(baseClass, selector);
-    NSValue *impValue = [[_impLookupTable objectForKey:key] valueForKey:DZNSwizzleInfoPointerKey];
-    
-    // If the implementation for this class already exist, skip!!
-    if (impValue || !key || !baseClass) {
-        return;
-    }
-    
-    // Swizzle by injecting additional implementation
-    Method method = class_getInstanceMethod(baseClass, selector);
-    IMP dzn_newImplementation = method_setImplementation(method, (IMP)dzn_original_implementation);
-    
-    // Store the new implementation in the lookup table
-    NSDictionary *swizzledInfo = @{DZNSwizzleInfoOwnerKey: baseClass,
-                                   DZNSwizzleInfoSelectorKey: NSStringFromSelector(selector),
-                                   DZNSwizzleInfoPointerKey: [NSValue valueWithPointer:dzn_newImplementation]};
-    
-    [_impLookupTable setObject:swizzledInfo forKey:key];
 }
 
 
